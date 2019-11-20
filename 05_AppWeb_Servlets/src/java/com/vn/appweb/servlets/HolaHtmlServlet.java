1160b59afde4bf2af4e01d26f6726ee6258f88aa
jorge <jorge.garcia.lalin@gmail.com>
2019-11-20 18:01:56 +0100

    servlets

---
+++ 05_AppWeb_Servlets/src/java/com/vn/appweb/servlets/HolaHtmlServlet.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vn.appweb.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * servlet que genera una respuesta en formato html
+ *
+ * @author PC
+ */
+public class HolaHtmlServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest peticionHttp,
+            HttpServletResponse respuestaHttp) throws IOException {
+        //definimos el tipo de contenido segun los tipos MINE
+        //formatos conocidos de ficheros para emails
+        respuestaHttp.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter salida = respuestaHttp.getWriter()) {
+
+            salida.print("<html>");
+            salida.print("<head>");
+            salida.print("<title>Web html desde Servlet</title>");
+            salida.print("<link  rel='stylesheet' href='/AppWebServlets/saludos/holaCSS'/>");
+            salida.print("</head>");
+            salida.print("<body>"
+                    + "<h1>web HTML desde servlets</h1>"
+                    + "<h2>Hola que pasa</h2>"
+                    + "<ul>");
+            for (int i = 0; i < 10; i++) {
+                salida.print("<li class='tamaño"+i+"'>Iteracion " + i + "</li>");
+            }
+            salida.print("</ul>");
+            salida.print("</br>Ruta: "+ peticionHttp.getContextPath());
+            salida.print("</br>Metodos: "+ peticionHttp.getMethod());
+            salida.print("</body>");
+            salida.print("<html>");
+            //salida.close(); //ya no hace falta ya que el try lo cierra
+        }
+    }
+}
--- 05_HTML5/jsjQuery/10_jQueryDOM.js
+++ 05_HTML5/jsjQuery/10_jQueryDOM.js
@@ -1,12 +1,36 @@
-jQuery(document).read(function () {
+jQuery(document).ready(function () {
     jQuery("section").append("<div><p>leyenda de lo que sea</p></div>");
 
     $("tr").append("<td>infoz</td>");
-    $("tr:nth-child(5)").attr("style","background-color: red, color white");
-    $ ("#otro_menu").click(function(){
-        $(this).slideUp();
+    $("tr:nth-child(5)").attr("style", "background-color: red, color white");
+
+    $(".h2").click(function () {
+        $(this).toggle(1000);
     });
-    $(".h2").click(function(){
-        $(this).slideUp();
+
+    $("article").each(function (index) {
+        $(this).hide(3000).slideUp();
+
+        let idArticulo = "articulo_29juR_" + index
+        
+        $(this).attr("id", idArticulo);
+        $("#otro_menu").append("<a href='#" + idArticulo + "'class='enlace-articulo'>Articulo nº " + index + "</a>");
+
     });
+    $(".enlace-articulo").each(function (index) {
+        $(this).click(function () {
+
+
+            $("#articulo_29juR_"+index).toggle(3000);
+
+        });
+
+    });
+
+
+    //ahora lo que hay que haceer es lo siguiente:
+    //Esconder los articulos al cargar la pagina
+    //luego al pulsar en el enlace de arriba
+    //que aparezcan los articulos y te lleve a el articulo abierto
+
 });
